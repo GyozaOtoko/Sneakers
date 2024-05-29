76e451fa98373d4d7a98346e7101ba055e5fc68e
Jay Bandeira <133895259+GyozaOtoko@users.noreply.github.com>
2024-05-29 03:44:59 -0400

    Added menu

--- sneakers-btc/src/components/HelloWorld.vue
+++ sneakers-btc/src/components/HelloWorld.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="background-images">
-    <img src="@/assets/images/1x1.svg">
-    <img src="@/assets/images/1x2.svg">
-    <img src="@/assets/images/1x3.svg">
-    <img src="@/assets/images/1x4.svg">
+  <div style="position: absolute; width: 100%; top: 0">
+    <img style="position: absolute; left: 0" src="@/assets/images/1x1.svg">
+    <img style="position: absolute; left: 0" src="@/assets/images/1x2.svg">
+    <img style="position: absolute; right: 0" src="@/assets/images/1x3.svg">
+    <img style="position: absolute; right: 0" src="@/assets/images/1x4.svg">
   </div>
+  <v-app-bar :elevation="0" color="rgba(0,0,0,0)">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <!-- <v-app-bar-title>Application Bar</v-app-bar-title> -->
+
+    <v-toolbar-items class="hidden-xs-only">
+      <template v-for="(item, i) in menuItems" :key="i">
+        <v-hover v-slot="{ isHovering, props }">
+          <v-btn flat :to="test" variant="plain" :class="isHovering ? 'menuHover' : ''" v-bind="props">{{ item.title }}</v-btn>
+        </v-hover>
+      </template>
+    </v-toolbar-items>
+
+    <template v-slot:append>
+      <v-btn icon="mdi-discord" href="https://x.com/BitcoinSneakers" target="_blank"></v-btn>
+      
+      <v-btn icon="svg:M2.04875 3.00002L9.77052 13.3248L1.99998 21.7192H3.74882L10.5519 14.3697L16.0486 21.7192H22L13.8437 10.8137L21.0765 3.00002H19.3277L13.0624 9.76874L8.0001 3.00002H2.04875ZM4.62054 4.28821H7.35461L19.4278 20.4308H16.6937L4.62054 4.28821Z" href="https://x.com/BitcoinSneakers" target="_blank"></v-btn>
+
+      <!-- <v-btn icon="mdi-dots-vertical"></v-btn> -->
+    </template>
+  </v-app-bar>
   <v-container class="fill-height justify-center">
-    <div style="max-width: 1646px; max-height: 870px">
+    <div style="max-width: 1646px; max-height: 870px; position: relative">
       <v-sheet :elevation="1" height="70vh" color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 70vh; display: flex">
         <div style="display: flex; margin: 50px;">
           <div class="align-center" style="display: flex">
@@ -51,7 +74,15 @@
 </template>
 
 <script setup lang="ts">
-  //
+  const sidebar = false;
+  const menuItems = [
+    { title: 'Home', path: '/home', icon: 'home' },
+    { title: 'About', path: '/signup', icon: 'face' },
+    { title: 'Team', path: '/signin', icon: 'lock_open' },
+    { title: 'Collections', path: '/signin', icon: 'lock_open' },
+    { title: 'Shop', path: '/signin', icon: 'lock_open' }
+  ]
+
 </script>
 
 <style>
@@ -104,5 +135,11 @@
 .squiggle_sm-bg {
   position: absolute; 
   background-image: url('@/assets/images/squiggle_sm2.png');
+}
+.v-app-bar.v-toolbar {
+  background-color: rgba(0,0,0,0);
+}
+.menuHover * {
+  color: #E87A0A;
 }
 </style>
