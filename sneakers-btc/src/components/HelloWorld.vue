e19c68954d138a41d6b2452ec3f081f58dd5a9da
Jay Bandeira <133895259+GyozaOtoko@users.noreply.github.com>
2024-05-31 12:18:12 -0400

    Started collections page and added metadata.json file

--- sneakers-btc/src/components/HelloWorld.vue
+++ sneakers-btc/src/components/HelloWorld.vue
@@ -9,13 +9,11 @@
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </template>
-
     <!-- <v-app-bar-title>Application Bar</v-app-bar-title> -->
-
     <v-toolbar-items class="hidden-xs-only">
       <template v-for="(item, i) in menuItems" :key="i">
         <v-hover v-slot="{ isHovering, props }">
-          <v-btn flat :to="test" variant="plain" :class="isHovering ? 'menuHover' : ''" v-bind="props">{{ item.title }}</v-btn>
+          <v-btn flat :to="item.path" variant="plain" :class="isHovering ? 'menuHover' : ''" v-bind="props">{{ item.title }}</v-btn>
         </v-hover>
       </template>
     </v-toolbar-items>
@@ -29,7 +27,7 @@
     </template>
   </v-app-bar>
   <v-container class="fill-height justify-center">
-    <div style="max-width: 1646px; max-height: 870px; position: relative">
+    <div id="home" style="max-width: 1646px; max-height: 870px; position: relative">
       <v-sheet :elevation="1" height="70vh" color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 70vh; display: flex">
         <div style="display: flex; margin: 50px;">
           <div class="align-center" style="display: flex">
@@ -61,7 +59,7 @@
         </div>
       </v-sheet>
     </div>
-    <v-container class="justify-center justify-center d-flex">
+    <v-container class="justify-center justify-center d-flex" style="margin-top: 50px">
       <div style="max-width: 1646px;max-height: 870px;display: flex;flex-direction: column;align-items: center;gap: 5px;cursor: pointer">
         <div style="font-size: 20px">Scroll down</div>
         <div>
@@ -70,19 +68,133 @@
         </div>
       </div>
     </v-container>
+    <v-container class="justify-center d-flex" style="margin-top: 150px">
+      <div id="collections" style="max-width: 1646px;max-height: 870px;display: flex;flex-direction: column;">
+        <h2 class="title">Collections</h2>
+        <div class="stats d-flex" style="gap: 10px; margin-top: 50px">
+          <div id="search" class="coll-container">
+            <div class="coll_title">Bitcoin<br/>sneakers</div>
+            <div class="coll_date">Created <b>March 11, 2023</b></div>
+            <div class="coll_filter">
+            <!-- <v-btn @click="dialog = true">Filter</v-btn>
+              <v-dialog v-model="dialog" width="auto">
+                <v-card
+                  max-width="400"
+                  prepend-icon="mdi-update"
+                  text="Your application will relaunch automatically after the update is complete."
+                  title="Update in progress"
+                ></v-card> -->
+                <!-- <template v-for="(item, index) in traitCategories"
+                    :key="index"
+                    :value="index">
+                    <v-select
+                      chips
+                      :items="item.values"
+                      :label="item.traitType"
+                      multiple
+                    ></v-select>
+                </template> -->
+                <!-- <template v-slot:actions>
+                  <v-btn
+                  class="ms-auto"
+                  text="Ok"
+                  @click="dialog = false"
+                  ></v-btn>
+                </template>
+              </v-dialog> -->
+              <v-dialog max-width="500">
+                <template v-slot:activator="{ props: activatorProps }">
+                  <v-btn
+                    v-bind="activatorProps"
+                    color="surface-variant"
+                    text="Filter"
+                    variant="flat"
+                  ></v-btn>
+                </template>
+
+                <template v-slot:default="{ isActive }">
+                  <v-card title="Traits">
+                    <v-card-text>
+                      <template v-for="(item, index) in traitCategories"
+                          :key="index"
+                          :value="index">
+                          <v-select
+                            chips
+                            clearable
+                            :items="item.values"
+                            :label="item.traitType"
+                            multiple
+                          ></v-select>
+                      </template>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+
+                      <v-btn
+                        text="Close"
+                        @click="isActive.value = false"
+                      ></v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </div>
+            <div class="coll_search">search</div>
+          </div>
+        </div>
+      </div>
+    </v-container>
   </v-container>
 </template>
 
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import jsonData from '@/assets/json/pioneersMetadata.json'
+  
+  let dialog = false;
   const sidebar = false;
   const menuItems = [
-    { title: 'Home', path: '/home', icon: 'home' },
-    { title: 'About', path: '/signup', icon: 'face' },
-    { title: 'Team', path: '/signin', icon: 'lock_open' },
-    { title: 'Collections', path: '/signin', icon: 'lock_open' },
-    { title: 'Shop', path: '/signin', icon: 'lock_open' }
-  ]
+    { title: 'Home', path: '#home', icon: 'home' },
+    { title: 'About', path: '#about', icon: 'face' },
+    { title: 'Team', path: '#team', icon: 'lock_open' },
+    { title: 'Collections', path: '#collections', icon: 'lock_open' },
+    { title: 'Shop', path: '#shop', icon: 'lock_open' }
+  ];
 
+  // let traitCategories: [{
+  //   [key: string] : {
+  //     traitType: string,
+  //     values: [string]
+  //   }
+  // }] | any = [];
+
+  let traitCategories: [{
+      traitType: string,
+      values: [string]
+  }] | any = [];
+  
+  onMounted(() => {
+    //build traitCategories
+    jsonData.forEach(item => {
+      item.meta.attributes.forEach((att: {trait_type: string, value: string}) => {
+        if(!traitCategories.find(item => item.traitType == att.trait_type)) {
+          traitCategories.push({
+            traitType: att.trait_type, 
+            values: [att.value]
+          })
+        }
+        else {
+          let tempTraitItem = traitCategories.find(item => item.traitType == att.trait_type);
+          if (!tempTraitItem.values.includes(att.value)) {
+            tempTraitItem.values.push(att.value);
+          }
+        }
+      })
+    })
+    console.log(JSON.stringify(traitCategories))
+     // <div>
+  })
 </script>
 
 <style>
@@ -141,5 +253,17 @@
 }
 .menuHover * {
   color: #E87A0A;
+}
+.coll_title { grid-area: title; font: normal normal bold 30px/30px ITC Avant Garde Gothic W02 Md; letter-spacing: 2px;}
+.coll_date { grid-area: date; font: normal normal normal 16px/17px Helvetica Neue; letter-spacing: -0.32px;}
+.coll_filter { grid-area: filter; }
+.coll_search { grid-area: search; }
+.coll-container {
+  display: grid;
+  grid-template-areas:
+    'title date'
+    'title filter'
+    'search search';
+  gap: 10px;
 }
 </style>
