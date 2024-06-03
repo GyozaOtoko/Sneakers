2bc7af3e484b3935b8a8930c03a592c50a8c0ff6
Jay Bandeira <133895259+GyozaOtoko@users.noreply.github.com>
2024-06-03 13:33:07 -0400

    fixed about

--- sneakers-btc/src/components/About.vue
+++ sneakers-btc/src/components/About.vue
@@ -1,15 +1,13 @@
 <template>
-    <div id="about" style="max-width: 1646px; max-height: 870px; position: relative">
-        <v-container class="justify-center" style="text-align: center; height: 60vh; ">
-      <v-container :elevation="1" class="justify-center" style="text-align: center;">               
-        <v-row justify="center">          
+    <div id="about" style="max-width: 1400px; position: relative; margin-top: 200px">
+      <v-container class="justify-center" style="text-align: center;">       
+        <v-row justify="center">
             <div style="margin-bottom: 20px; z-index: 1000">
-              <span class="about-title" style="margin-bottom: 0;  ">About</span>
+              <span class="about-title">About</span>
             </div>
-          
         </v-row>
-        <v-container :elevation="2" class="justify-center d-flex" style="text-align: center; min-height: 125px; margin-bottom: -75px;">
-          <v-row justify="center">
+        <v-container style="height: 125px; margin-bottom: -85px;">
+          <v-row justify="center" style="height: 125px">
               <v-col cols="1"></v-col>
               <v-col cols="3" class="orange-arrow1"></v-col>
               <v-col cols="3" class="orange-arrow-squiggle"></v-col>
@@ -19,56 +17,45 @@
         </v-container>
       <v-row class="mb-4">
         <v-col>
-          <div style="max-width: 337px; max-height: 265px; position: relative; margin: auto;">
-            <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 40vh; display: flex; align-items: center; justify-content: center;">
-              <div style="padding: 30px;" class="about-text">
-                <span>Bitcoin Sneakers is a project at the crossroads of fashion, art, and technology.</span>
-              </div>
-            </v-sheet>
-          </div>
+          <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded class="about-card" style="">
+            <div style="padding: 30px;" class="about-text">
+              <span class="text">Bitcoin Sneakers is a project at the crossroads of fashion, art, and technology.</span>
+            </div>
+          </v-sheet>
         </v-col>
         <v-col>
-          <div style="max-width: 337px; max-height: 265px; position: relative; margin: auto;">
-            <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 40vh; display: flex; align-items: center; justify-content: center;">
-              <div style="padding: 30px;" class="about-text">
-                <span>On February 9th 2023 they made history by becoming the first fashion collection inscribed on Bitcoin.</span>
-              </div>
-            </v-sheet>
-          </div>
+          <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded class="about-card" style="">
+            <div style="padding: 30px;" class="about-text">
+              <span class="text">On February 9th 2023 they made history by becoming the first fashion collection inscribed on Bitcoin.</span>
+            </div>
+          </v-sheet>
         </v-col>
         <v-col>
-          <div style="max-width: 337px; max-height: 265px; position: relative; margin: auto;">
-            <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 40vh; display: flex; align-items: center; justify-content: center;">
-              <div style="padding: 30px;" class="about-text">
-                <span>Following the success of their genesis collection, they embarked on their next venture—a PFP collection designed to be the face of the brand—the Pioneers.</span>
-              </div>
-            </v-sheet>
-          </div>
+          <v-sheet :elevation="1"  color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded class="about-card" style="">
+            <div style="padding: 30px;" class="about-text">
+              <span class="text">Following the success of their genesis collection, they embarked on their next venture—a PFP collection designed to be the face of the brand—the Pioneers.</span>
+            </div>
+          </v-sheet>
         </v-col>
         <v-col>
-          <div style="max-width: 337px; max-height: 265px; position: relative; margin: auto;">
-            <v-sheet :elevation="1" color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded style="border-radius: 30px; border: 1px solid #707070; height: 40vh; display: flex; align-items: center; justify-content: center;">
-              <div style="padding: 30px;" class="about-text">
-                <span>With a limited supply of 888, they quickly sold out, fueling the momentum for their next project: IRL sneakers.</span>
-              </div>
-            </v-sheet>
-          </div>
+          <v-sheet :elevation="1" color="linear-gradient(180deg, #262626 0%, #33333E 100%)" rounded class="about-card" style="">
+            <div style="padding: 30px;" class="about-text">
+              <span class="text">With a limited supply of 888, they quickly sold out, fueling the momentum for their next project: IRL sneakers.</span>
+            </div>
+          </v-sheet>
         </v-col>
       </v-row>           
-    </v-container>    
-  </v-container>
-  <v-container style="text-align: center;  overflow-y: auto;" class="flex-row">
-      <v-row justify="center" >        
-        <v-col cols="7">          
-          <div>
-            <span class="text" style="">Join them as they bridge the gap between the digital and physical realms through the fusion of fashion, art, and technology.</span>
-          </div>
-        
-        </v-col>        
-      </v-row>
-  </v-container>
- 
-    </div>
+    </v-container>
+    <v-container style="text-align: center;  overflow-y: auto;" class="flex-row">
+        <v-row justify="center" >        
+          <v-col cols="7">          
+            <div>
+              <span>Join them as they bridge the gap between the digital and physical realms through the fusion of fashion, art, and technology.</span>
+            </div>
+          </v-col>        
+        </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -83,25 +70,32 @@
   opacity: 1;
   z-index: 1000;
 }
+.about-card {
+  border-radius: 30px; border: 1px solid #707070; display: flex; align-items: center; justify-content: center;
+  height: 100%;
+}
 .orange-arrow1 {
   background-image: url('@/assets/images/orange_arrow1.svg');
   background-position: center;
   background-size: contain;
-  height: 125%;
+  height: 100%;
   z-index: 1000;
+  transform: scale(0.7);
 }
 .orange-arrow2 {
   background-image: url('@/assets/images/orange_arrow2.svg');
   background-position: center;
   background-size: contain;
-  height: 125%;
+  height: 100%;
   z-index: 1000;
+  transform: scale(0.7) translateX(20px);
 }
 .orange-arrow-squiggle {
   background-image: url('@/assets/images/orange_arrow_sqiggle.svg');
   background-position: center;
   background-size: contain;
-  height: 125%;
+  height: 100%;
   z-index: 1000;
+  transform: scale(1.2) translate(-4px, 10px);
 }
 </style>
--- sneakers-btc/src/components/HelloWorld.vue
+++ sneakers-btc/src/components/HelloWorld.vue
@@ -6,39 +6,31 @@
     <img style="position: absolute; right: 0" src="@/assets/images/1x4.svg">
   </div>
   <v-container class="fill-height justify-center">
-    
     <HeroSplash />
-    
-  <v-container class="justify-center d-flex" >
-    <About />
-  </v-container>
-
-  <v-container class="justify-center d-flex" >
-    <Team />
-  </v-container>
-
-  <v-container class="justify-center d-flex" >
-    <ShoeSizes />
-  </v-container>
-    
-
-   
+    <v-container class="justify-center d-flex">
+      <About />
+    </v-container>
+    <v-container class="justify-center d-flex">
+      <Team />
+    </v-container>
+    <v-container class="justify-center d-flex">
+      <ShoeSizes />
+    </v-container>
     <!-- <ScrollDown /> -->
     <v-container class="justify-center d-flex" style="margin-top: 150px; position: relative">
       <div id="collections" style="max-width: 1400px; display: flex; flex-direction: column; align-items: center;">
         <h2 class="title">Collections</h2>
-          <Collections collection="sneakers"/>
-          <div style="margin: 50px 0"></div>
-          <Collections collection="pioneers"/>
+        <Collections collection="sneakers" />
+        <div style="margin: 50px 0"></div>
+        <Collections collection="pioneers" />
       </div>
     </v-container>
     <Shop />
- 
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //  
+//  
 </script>
 
 <style>
@@ -50,12 +42,14 @@
   letter-spacing: 2px;
   color: #FFFFFF;
 }
+
 .squiggle-sm {
   width: 100px;
 }
+
 .v-sheet.card-box {
   border-radius: 20px;
-  border: 1px solid #707070; 
+  border: 1px solid #707070;
   padding: 10px 15px;
   text-wrap: nowrap;
   text-align: center;
@@ -63,54 +57,68 @@
   flex-direction: column;
   justify-content: center;
 }
+
 .card_title {
   font: normal normal bold 14px Helvetica Neue;
   letter-spacing: -0.32px;
-  color:#E87A0A;
+  color: #E87A0A;
   align-items: center;
 }
+
 .card_title-units {
   font: normal normal bold 10px Helvetica Neue;
 }
+
 .card_subtitle {
   font: normal normal normal 8px Helvetica Neue;
   letter-spacing: -0.32px;
   color: #FFFFFF;
   opacity: 0.5;
 }
+
 @media (min-width: 960px) {
   .title {
     font: normal normal bold 40px ITC Avant Garde Gothic W02 Md;
   }
+
   .squiggle-sm {
     width: auto;
   }
+
   .v-sheet.card-box {
     padding: 15px 20px;
   }
+
   .card_title {
     font: normal normal bold 16px Helvetica Neue;
   }
+
   .card_title-units {
     font: normal normal bold 12px Helvetica Neue;
   }
+
   .card_subtitle {
     font: normal normal normal 10px Helvetica Neue;
   }
 }
+
 @media (min-width: 1280px) {
   .title {
     font: normal normal bold 50px/45px ITC Avant Garde Gothic W02 Md;
   }
+
   .v-sheet.card-box {
     padding: 20px 30px
   }
+
   .card_title {
     font: normal normal bold 26px/52px Helvetica Neue;
   }
+
   .card_title-units {
     font: normal normal bold 16px Helvetica Neue;
   }
+
   .card_subtitle {
     font: normal normal normal 12px Helvetica Neue;
   }
@@ -123,23 +131,27 @@
   color: #FFFFFF;
   opacity: 0.5;
 }
+
 .text {
   font: normal normal normal 16px/17px Helvetica Neue;
   letter-spacing: -0.32px;
   color: #FFFFFF;
   opacity: 0.5;
 }
+
 .vr {
-  margin: 0 30px; 
-  border-right: 2px solid #fff; 
+  margin: 0 30px;
+  border-right: 2px solid #fff;
   opacity: 0.5
 }
+
 .squiggly-bg {
   background-image: url('@/assets/images/sneakerSquiggle2.png');
   background-position: center;
   background-size: contain;
   height: 100%;
 }
+
 .sneaker-shadow {
   top: 760px;
   left: 363px;
@@ -149,20 +161,41 @@
   opacity: 0.5;
   filter: blur(5px);
 }
+
 .squiggle_sm-bg {
-  position: absolute; 
+  position: absolute;
   background-image: url('@/assets/images/squiggle_sm2.png');
 }
+
 .v-app-bar.v-toolbar {
-  background-color: rgba(0,0,0,0);
+  background-color: rgba(0, 0, 0, 0);
 }
+
 .menuHover * {
   color: #E87A0A;
 }
-.coll_title { grid-area: title; font: normal normal bold 30px/30px ITC Avant Garde Gothic W02 Md; letter-spacing: 2px; align-self: center}
-.coll_date { grid-area: date; font: normal normal normal 16px/17px Helvetica Neue; letter-spacing: -0.32px;}
-.coll_filter { grid-area: filter; }
-.coll_search { grid-area: search; }
+
+.coll_title {
+  grid-area: title;
+  font: normal normal bold 30px/30px ITC Avant Garde Gothic W02 Md;
+  letter-spacing: 2px;
+  align-self: center
+}
+
+.coll_date {
+  grid-area: date;
+  font: normal normal normal 16px/17px Helvetica Neue;
+  letter-spacing: -0.32px;
+}
+
+.coll_filter {
+  grid-area: filter;
+}
+
+.coll_search {
+  grid-area: search;
+}
+
 .coll-container {
   display: grid;
   grid-template-areas:
@@ -170,5 +203,7 @@
     'title filter'
     'title search';
   gap: 10px;
-};
+}
+
+;
 </style>
