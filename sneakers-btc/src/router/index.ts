efe0a98f8a850699fb434f3a3b642063ff9d059d
Jay Bandeira <133895259+GyozaOtoko@users.noreply.github.com>
2024-06-08 00:57:42 -0400

    Tried fixing navigation, no luck

--- sneakers-btc/src/router/index.ts
+++ sneakers-btc/src/router/index.ts
@@ -11,6 +11,15 @@ import { setupLayouts } from 'virtual:generated-layouts'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: setupLayouts,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    }
+    return savedPosition || { top: 0 };
+  },
 })
 
 export default router
